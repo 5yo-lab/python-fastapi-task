67526c0fc2101719e0f9b2612d57d7c54689e82f
petyo.kirilov <petiofiiito@gmail.com>
2026-08-26 16:54:49 +0300

    add routes for products and categories

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from app.routes.products import router as products_router
+from app.routes.categories import router as categories_router
 
 app = FastAPI()
+
+app.include_router(products_router)
+app.include_router(categories_router)
 
 @app.get("/")
 def index():
